28aca6bb0a7097d019b3692e6be8347a1075376e
Dave Chen <dchen@mail.nih.gov>
2019-10-16 15:07:32 -0400

    first working code

--- sitkwidget.py
+++ sitkwidget.py
@@ -6,11 +6,11 @@ import numpy as np
 import itkwidgets
 
 
-def itk_view_from_simpleitk(img):
-  np_view = sitk.GetArrayViewFromImage(img)
+def itk_view_from_simpleitk(image):
+  np_view = sitk.GetArrayViewFromImage(image)
   itk_view = itk.image_view_from_array(np_view)
   return itk_view
 
-def view(img):
-  itk_view = itk_view_from_simpleitk(img)
-  itkwidgets.view(itk_view)
+def view(image, *args, **kwargs):
+  itk_view = itk_view_from_simpleitk(image)
+  return itkwidgets.view(itk_view, *args, **kwargs)
