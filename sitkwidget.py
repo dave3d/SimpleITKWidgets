8e5770dc2b869aa9d2e5e3b70005e4a06364c3d0
Dave Chen <dchen@mail.nih.gov>
2019-10-28 15:06:04 -0400

    Copy meta info from SimpleITK image to ITK image
    It now copies the origin, spacing and direction image
    from the SimpleITK image to the ITK image.

--- sitkwidget.py
+++ sitkwidget.py
@@ -7,10 +7,32 @@ import itkwidgets
 
 
 def itk_view_from_simpleitk(image):
-#  TBD: copy all the meta information from the SimpleITK image to the ITK image
+
+  origin = image.GetOrigin()
+  spacing = image.GetSpacing()
+  direction = image.GetDirection()
+
   np_view = sitk.GetArrayViewFromImage(image)
   itk_view = itk.image_view_from_array(np_view)
+
+  itk_view.SetOrigin(origin)
+  itk_view.SetSpacing(spacing)
+
+  npdir = np.zeros([3,3])
+  npdir[0][0] = direction[0]
+  npdir[0][1] = direction[1]
+  npdir[0][2] = direction[2]
+  npdir[1][0] = direction[3]
+  npdir[1][1] = direction[4]
+  npdir[1][2] = direction[5]
+  npdir[2][0] = direction[6]
+  npdir[2][1] = direction[7]
+  npdir[2][2] = direction[8]
+  itkdir = itk.matrix_from_array(npdir)
+  itk_view.SetDirection(itkdir)
+
   return itk_view
+
 
 def view(image, *args, **kwargs):
   itk_view = itk_view_from_simpleitk(image)
